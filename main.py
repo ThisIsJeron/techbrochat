2f3618d724bc925850254e558263de5bed4236f2
Jeron Wong <ThisIsJeron@users.noreply.github.com>
2023-11-01 10:18:08 -0700

    update prompt

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import openai
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
 
 # Set your API key here (preferably as an environment variable for security reasons)
 API_KEY = os.getenv('API_KEY')
@@ -14,7 +11,7 @@ def get_response_from_gpt(prompt_text):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-        {"role": "system", "content": "You are a listener of a lifestyle podcast from two San Francisco bros that are software engineers"},
+        {"role": "system", "content": "You are a listener of a lifestyle podcast from two San Francisco bros"},
         {"role": "user", "content": prompt_text}
     ]
     )
@@ -22,7 +19,16 @@ def get_response_from_gpt(prompt_text):
     return response['choices'][0]['message']['content']
 
 if __name__ == "__main__":
-    question = "What are 5 topics you want to hear from the podcast today?"
-    answer = get_response_from_gpt(question)
+    usedTopics = ["day in the life of a software engineer",
+                  "dating in san francisco",
+                  "healthy work life balance",
+                  "digital nomadism"
+                  ]
+    boilerplate1 = "these are topics we have discussed in the past: "
+    boilerplate2 = "not repeating any of the previous topics, what are 3 topics you want to hear from the podcast today?"
+    
+    prompt = boilerplate1 + ', '.join(str(e) for e in usedTopics) + "\n" + boilerplate2
+    print(prompt)
+    answer = get_response_from_gpt(prompt)
     print(answer)
 
