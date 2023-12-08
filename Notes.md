98c87d6fdc29eeaffa142c34ddb460a72d1b828c
Jeron Wong <ThisIsJeron@users.noreply.github.com>
2023-12-07 23:47:02 -0800

    update main

---
+++ Notes.md
@@ -0,0 +1,9 @@
+Notes
+
+1. Intro - techbro.chat
+    a. what is this podcast
+    b. who are we
+
+1. explain our topic script
+
+1. talk about topics
--- main.py
+++ main.py
@@ -7,27 +7,30 @@ API_KEY = os.getenv('API_KEY')
 def get_response_from_gpt(prompt_text):
     openai.api_key = API_KEY
     
-    
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4",
         messages=[
         {"role": "system", "content": "You are a listener of a lifestyle podcast from two San Francisco bros"},
         {"role": "user", "content": prompt_text}
-    ]
+        ]
     )
+    
     print(response)
     return response['choices'][0]['message']['content']
 
 if __name__ == "__main__":
-    usedTopics = ["day in the life of a software engineer",
-                  "dating in san francisco",
-                  "healthy work life balance",
-                  "digital nomadism"
-                  ]
     boilerplate1 = "these are topics we have discussed in the past: "
     boilerplate2 = "not repeating any of the previous topics, what are 3 topics you want to hear from the podcast today?"
     
-    prompt = boilerplate1 + ', '.join(str(e) for e in usedTopics) + "\n" + boilerplate2
+    #open usedTopics.txt, read the topics, and store them in a list
+    usedTopics = []
+    with open('usedTopics.txt', 'r') as f:
+        usedTopics = f.read().splitlines()
+
+
+    prompt = boilerplate1 + ', '.join(usedTopics) + "\n" + boilerplate2
+
+
     print(prompt)
     answer = get_response_from_gpt(prompt)
     print(answer)
